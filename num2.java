db9a6b85d1c71a332ae2959782dbc58faa9ea3b0
Perry E. Henderson <hendersonperry96@yahoo.com>
2017-07-01 19:18:28 -0400

    first commit

---
+++ num2.java
@@ -0,0 +1,15 @@
+class PrimeExample{  
+ public static void main(String args[]){  
+  int i,m=0,flag=0;    
+  int n=17;
+  for(i=2;i<=m;i++){    
+   if(n%i==0){    
+   System.out.println("Not a prime number dude");    
+   flag=1;    
+   break;    
+   }    
+  }    
+  if(flag==0)    
+  System.out.println(" Prime number amigo!!!");    
+}  
+}  
